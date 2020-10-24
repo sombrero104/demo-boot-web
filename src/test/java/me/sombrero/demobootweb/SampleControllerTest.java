4b3e3d784f1a4239124b2c1505d787bdda1611d8
sombrero104 <sombrero.khy@gmail.com>
2020-10-24 19:11:10 +0900

    .

--- src/test/java/me/sombrero/demobootweb/SampleControllerTest.java
+++ src/test/java/me/sombrero/demobootweb/SampleControllerTest.java
@@ -8,14 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 // @WebMvcTest // 웹과 관련된 빈들만 등록해줌.
@@ -57,6 +57,15 @@ public class SampleControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(Matchers.containsString("hello index")));
+    }
+
+    @Test
+    public void helloMobile() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Hello Mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
     }
 
 }
